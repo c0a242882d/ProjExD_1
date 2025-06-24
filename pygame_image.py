2d871911fd29fe776d048d23328e3652e340820d
sawa <c0a242882d@edu.teu.ac.jp>
2025-06-24 13:02:11 +0900

    練習4

--- pygame_image.py
+++ pygame_image.py
@@ -12,6 +12,8 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像の読み込み
     kk_img = pg.transform.flip(kk_img, True, False)  # 左右反転
+    kk_rct = kk_img.get_rect()  # こうかとん画像のRect
+    kk_rct.center = 300, 200  # こうかとん画像の中心座標を設定
     tmr = 0
     while True:
         for event in pg.event.get():
