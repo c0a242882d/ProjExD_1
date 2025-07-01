086c5b6fff504ca02401d2cc63d10b3ef8b19866
sawa <c0a242882d@edu.teu.ac.jp>
2025-07-01 13:00:48 +0900

    演習1

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -9,34 +9,36 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像のSurface
-    bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を反転
-    kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
-    kk_img = pg.transform.flip(kk_img, True, False)
-    kk_rct = kk_img.get_rect()  # こうかとん画像のRect
-    kk_rct.center = 300, 200  # こうかとん画像の中心座標を設定
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")#こうかとんの画像Surface
+    kk_img=pg.transform.flip(kk_img,True,False)
+    bg_img2=pg.transform.flip(bg_img,True,False)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()  # キーの押下状態を取得
-        if key_lst[pg.K_UP]:  # 上キーが押されたら
-            kk_rct.move_ip(0, -1)  # 上に移動
+        x1=0
+        y1=0    
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            y1-=1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            y1+=1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            x1-=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
-
-        x = tmr%3200
-        screen.blit(bg_img, [-x, 0])  # 1枚目
-        screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
-        screen.blit(bg_img, [-x+3200, 0])  # 3枚目
-        # screen.blit(kk_img, [300, 200])
+           x1+=1
+        else:
+            x1-=1 
+        kk_rct.move_ip((x1,y1))  
+        x=tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        # print(tmr, x)
         tmr += 1        
         clock.tick(200)
 
