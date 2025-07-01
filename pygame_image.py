8209d91bb1c1effc07d62ca960c6a24c0cfd13bd
sawa <c0a242882d@edu.teu.ac.jp>
2025-07-01 11:52:09 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -9,8 +9,9 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")  # こうかとん画像の読み込み
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像のSurface
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を反転
+    kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img, True, False)  # 左右反転
     kk_rct = kk_img.get_rect()  # こうかとん画像のRect
     kk_rct.center = 300, 200  # こうかとん画像の中心座標を設定
@@ -19,7 +20,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])  # 1枚目
+        screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
+        screen.blit(bg_img, [-x+3200, 0])  # 3枚目
+
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(10)
